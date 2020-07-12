ac8a5719191ed7ba2bbab082d10d613e5884a897
RiczCalixto <rcalixtos@gmail.com>
2020-07-11 21:59:55 -0300

    Create relation between provider_id and User table

--- src/database/migrations/1593984997124-CreateAppointments.ts
+++ src/database/migrations/1593984997124-CreateAppointments.ts
@@ -8,7 +8,7 @@ export class CreateAppointments1593984997124 implements MigrationInterface {
         columns: [
           {
             name: 'id',
-            type: 'varchar',
+            type: 'uuid',
             isPrimary: true,
             generationStrategy: 'uuid',
             default: 'uuid_generate_v4()',
--- src/database/migrations/1594505441860-CreateUser.ts
+++ src/database/migrations/1594505441860-CreateUser.ts
@@ -8,7 +8,7 @@ export class CreateUser1594505441860 implements MigrationInterface {
         columns: [
           {
             name: 'id',
-            type: 'varchar',
+            type: 'uuid',
             isPrimary: true,
             generationStrategy: 'uuid',
             default: 'uuid_generate_v4()',
